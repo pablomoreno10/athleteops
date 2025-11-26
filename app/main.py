c09c6885690351940e620fcae692b27feaf05df2
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-11-26 00:47:18 -0500

    implement and tested basic transaction routes

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from app.routes.health import router as health_router
 from app.routes.tasks import router as tasks_router
+from app.routes.transaction import router as finance_router
 
 app = FastAPI(title="AthleteOps API")
 
 app.include_router(tasks_router)
 app.include_router(health_router)
+app.include_router(finance_router)
 
 
 @app.get("/health", tags=["meta"])
---
+++ app/routes/transaction.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta, timezone
+
+from app.db import get_db
+
+from app.models import Transactions
+from app.schemas import TransactionCreate, TransactionRead
+from app.enums import TransactionCategory
+
+router = APIRouter(prefix="/finance", tags=["finance"])
+
+
+DEFAULT_USER_ID = 1
+
+#manually add a transaction
+@router.post('/transactions', response_model=TransactionRead)
+def create_transaction(transaction_in: TransactionCreate, db: Session=Depends(get_db)):
+        transaction_out = Transactions(
+                user_id=DEFAULT_USER_ID,
+                **transaction_in.model_dump(),
+                #model_dump used to serialize the pydantic model into a python dictionary
+        )
+        db.add(transaction_out)
+        db.commit()
+        db.refresh(transaction_out)
+        return transaction_out
+
+#get transactions no latest than one week ago
+@router.get("/transactions/week", response_model=list[TransactionRead])
+def list_transactions(days_ago: int = Query(7, ge=1, description="Number of days to look back"), db: Session=Depends(get_db)):
+    now = datetime.now(timezone.utc)
+    start_date = now - timedelta(days=days_ago)
+    weekly_transactions = db.query(Transactions).filter(Transactions.time_created >= start_date).all()
+    return weekly_transactions
